2668a63b8aa56701d5ae87f936ff0932018f66b2
Avinash kumar <avi87793919@gmail.com>
2025-10-14 00:22:16 +0530

    Complete the sequelize basics setup of db

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const {PORT} = require("./config/serverConfig");
+
+
+const setupAndStartServer = () =>{
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    const PORT = 3000
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    }); 
+}
+
+setupAndStartServer();
