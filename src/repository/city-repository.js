70f306b227e4b6c69811ebdc616269f62ad90223
Avinash kumar <avi87793919@gmail.com>
2025-12-14 11:49:39 +0530

    Added city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,24 +1,57 @@
+const { where } = require('sequelize');
 const {City} = require('../models/index');
 
 class CityRepository {
+
+
     async createCity({ name }){
-        try{
-            const city = await City.create({name});
+        try {
+            const city = await City.create({
+                name
+            });
             return city;
-        }catch(error){
+        } catch (error) {
+            console.log("something went wrong in repository layer");
             throw {error};
+            
         }
     }
 
-    async deleteCity({ cityId }){
+    async deleteCity({cityId}){
         try {
             await City.destroy({
                 where: {
                     id: cityId
                 }
-            })
+            });
+            return true;
         } catch (error) {
-            throw {error};
+            console.log("Somethig went wrong in repository layer");
+            throw{error};
+            
+        }
+    }
+
+    async updateCity({cityId, data}){
+        try {
+            const city = await City.update(data,{
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            
+        }
+    }
+
+    async getCity({cityId}){
+        try {
+            const city = await City.findByPk(cityId);
+            return City;
+        } catch (error) {
+            console.log("Somethig went wrong in repository layer");
+            throw{error};
         }
     }
 }
